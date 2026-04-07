2a455fe36c75354896f6d46af6fe628f98446035
javiersaceda <javiersaceda@gmail.com>
2026-04-07 23:28:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+var CACHE = 'labuenatletra-v1';
+
+var PRECACHE = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(PRECACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        return caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      }).catch(function() {
+        return cached;
+      });
+    })
+  );
+});
